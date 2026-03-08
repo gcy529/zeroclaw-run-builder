9bf710a7a00ef8b3ffeaad832b82f062861d433b
gcy529 <gcy529@users.noreply.github.com>
2026-03-08 22:55:04 +0800

    fix: 添加 LuCI 新版界面支持

---
+++ openwrt-zeroclaw/luci-app-zeroclaw/htdocs/luci-static/resources/view/zeroclaw/settings.js
@@ -0,0 +1,36 @@
+'use strict';
+return renderView-main extends Vue
+{
+    data() {
+        return {
+            config: {},
+            loading: true,
+            saving: false
+        };
+    },
+    mounted() {
+        this.loadConfig();
+    },
+    methods: {
+        async loadConfig() {
+            this.loading = true;
+            try {
+                const res = await L.api.call('luci.zeroclaw.getConfig');
+                this.config = res || {};
+            } catch (e) {
+                console.error(e);
+            }
+            this.loading = false;
+        },
+        async saveConfig() {
+            this.saving = true;
+            try {
+                await L.api.call('luci.zeroclaw.setConfig', this.config);
+                L.ui.addNotification(null, _('Settings saved'), 'success');
+            } catch (e) {
+                L.ui.addNotification(null, _('Save failed: %s'.format(e.message)), 'danger');
+            }
+            this.saving = false;
+        }
+    }
+};
